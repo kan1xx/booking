27e75d51ec3d2c82ba4105cd723329fde7319f11
Miroslav <fedechkin.miroslav@yandex.ru>
2025-07-08 19:29:07 +0200

    function for logout and get info about current user

--- app/users/router.py
+++ app/users/router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from app.users.auth import get_password_hash
 from app.users.dao import UsersDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import SUserAuth
 from app.users.auth import authenticate_user, create_access_token
 
@@ -27,3 +29,12 @@ async def login_user(response: Response, user_data: SUserAuth):
     access_token = create_access_token({'sub': str(user.id)})
     response.set_cookie('booking_access_token', access_token, httponly=True)
     return access_token
+
+
+@router.post('/logout')
+async def logout_user(response: Response):
+    response.delete_cookie('booking_access_token')
+    
+@router.get('/me')
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
