1f3878604faac303363e8559ec9aa892c4793081
Miroslav <fedechkin.miroslav@yandex.ru>
2025-07-08 19:02:14 +0200

    secret_key, algorithm

--- app/config.py
+++ app/config.py
@@ -8,11 +8,15 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
-    model_config = ConfigDict(env_file='.env')
-
     @computed_field
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = ConfigDict(env_file='.env')
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
 
 settings = Settings()
